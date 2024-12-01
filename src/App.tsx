0f0a953bad037b6a53cb46fe6bc54cfb58db4c03
nFd_noFugaziDev <mubaraqmuhammad06@gmail.com>
2024-12-01 15:01:30 +0100

    navbar: implemented navbar and its responsivesness

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar";
+import styled from "styled-components";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-    </div>
-  )
-}
+    <Container>
+      <div className="top bg-[#333333] px-5 py-3 sticky top-0">
+        <Navbar />
+      </div>
+    </Container>
+  );
+};
 
-export default App
+export default App;
+
+
+const Container = styled.div`
+  margin: 0 auto;
+`
+
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,19 +4,19 @@ import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="flex flex-row items-center mx-auto max-w-[1200px]">
       {/* logo */}
-      <div>
-        <div className="logo">
-          <h1>fm</h1>
+      <div className="flex-[0.5] flex items-center gap-2 md:gap-3">
+        <div className="logo flex items-center justify-center border-none rounded-full bg-[#f1f1f1] p-1 md:p-3">
+          <h1 className="text-medium md:text-2xl font-medium md:font-extrabold text-[#333333] uppercase">fm</h1>
         </div>
-        <h1>FrontendMaster</h1>
+        <h1 className="text-white font-medium md:font-extrabold text-base md:text-2xl">FrontendMaster</h1>
       </div>
 
       {/* navigation Menu */}
 
-      <div>
-        <div className="nav-links">
+      <div className="flex-[0.5] md:flex md:items-center md:items-row md:justify-between">
+        <div className="nav-links hidden md:flex md:flex-row md:items-center md:gap-5 md:text-white md:capitalize md:text-lg md:font-light">
           <Link to="/">home</Link>
           <Link to="/">about</Link>
           <Link to="/">projects</Link>
@@ -25,10 +25,10 @@ const Navbar = () => {
           <Link to="/">contact</Link>
         </div>
 
-        <div className="socials">
-          <FaGithub />
-          <FaLinkedin />
-          <FaTwitter />
+        <div className="socials flex flex-row gap-3 justify-end">
+          <FaGithub className="text-white text-lg md:text-xl cursor-pointer" />
+          <FaLinkedin className="text-white text-lg md:text-xl cursor-pointer" />
+          <FaTwitter className="text-white text-lg md:text-xl cursor-pointer" />
         </div>
       </div>
     </nav>
